7ffd68a45a0096403947966865c6a03811fe5afe
andrey kurochkin <andrey.kurochkin@hotmail.com>
2022-07-06 14:52:43 +0300

    DefaultPasswordValidator tested

--- src/backend/Frame/Infrastructure/Validators/DefaultPasswordValidator.cs
+++ src/backend/Frame/Infrastructure/Validators/DefaultPasswordValidator.cs
@@ -15,11 +15,19 @@ public class DefaultPasswordValidator : IPasswordValidator
 
     public async Task<IdentityResult> ValidateAsync(Domain.IdentityUser user, string password)
     {
-        throw new NotImplementedException();
-        //var hashFromPersistent = user.Password;
-        //var salt = Encoding.ASCII.GetBytes(user.Salt);
-        //var hashToValidate = await Task.Run<string>( _hashProvider.GetHash(password, salt));
-
-        //var result = new IdentityResult
+        var hashFromPersistent = user.Password;
+        var salt = Encoding.ASCII.GetBytes(user.Salt);
+        var hashToValidate = await _hashProvider.GetHashAsync(password, salt);
+        var validated = hashFromPersistent == hashToValidate;
+        return validated
+            ? IdentityResult.Success 
+            : IdentityResult.Failed(new[]
+            {
+                new IdentityError
+                {
+                    Code = string.Empty,
+                    Description = "Bad login/password pair"
+                }
+            });
     }
 }
